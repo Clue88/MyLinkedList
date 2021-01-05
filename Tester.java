a7eef4f09ecec37032a51360fb8eb25c5ec003be
Christopher Liu <christopherliu@sorcero.com>
2021-01-05 14:20:38 -0500

    Add first tests

--- Tester.java
+++ Tester.java
@@ -5,7 +5,13 @@ public class Tester {
         if (args.length > 0 && Boolean.parseBoolean(args[0]) == false) DEBUG = false;
         String test = "";
 
-        
+        test = "Node(String value)";
+        try {
+            Node n = new Node("hello");
+            nothing(n);
+        } catch(RuntimeException e) {
+            except(test, e);
+        }
 
         if (ERR == 0) System.out.println("All good!");
         else if (ERR == 1) System.out.println("Uh oh... 1 error found.");
@@ -20,9 +26,28 @@ public class Tester {
         }
     }
 
+    public static void check(String test, Object actual, Object expected, int seed) {
+        if (!actual.equals(expected)) {
+            System.out.print("Failure on " + test + ": Expected \"" +
+                                expected + "\", got \"" + actual + "\".");
+            if (DEBUG) System.out.println(" Seed: " + seed);
+            else System.out.println();
+            ERR++;
+        }
+    }
+
     public static void except(String test, RuntimeException e) {
         System.out.println("Failure on " + test + ": RuntimeException");
         if (DEBUG) System.out.println(e.toString());
         ERR++;
+    }
+
+    public static void noException(String test, String expected) {
+        System.out.println("Failure on " + test + ": Expected " + expected);
+        ERR++;
+    }
+
+    public static void nothing(Object... nothings) {
+        return;
     }
 }
